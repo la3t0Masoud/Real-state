843e163c3d7e677ec1b4fa6262649897c1064659
la3t0Masoud <la3t.masoud11@gmail.com>
2025-10-29 16:27:24 +0330

    toastify applyed successfully

--- real-state/src/App.jsx
+++ real-state/src/App.jsx
@@ -4,11 +4,14 @@ import About from './components/About.jsx'
 import Projects from './components/Projects.jsx'
 import Testimonails from './components/Testimonails.jsx'
 import Contact from './components/Contact.jsx'
+import { ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 
 function App() {
   return (
     <div className='w-full overflow-hidden'>
+      <ToastContainer/>
       <Header/>
       <About/>
       <Projects/>
